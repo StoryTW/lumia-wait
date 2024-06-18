06978cc2fe1868af57db5643c3d480439cc2c1b9
parienko_sergej <parienko_sergej@enot.work>
2024-06-19 01:31:52 +0300

    fix

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/sergeyparienko/Desktop/my/lumia-wait/src/app/layout\":[\"static/media/ad1715a5ea03ce0e-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"k6TOn+OVEs2E1Lpln8YQoNiLmWmns9AWccXGogwrIH4=\"\n}"
